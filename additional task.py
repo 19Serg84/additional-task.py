25ee7d81fcb9c07f6e25283f04caa9115a9a39bb
19Serg84 <pagalin.sergey@yandex.ru>
2024-08-11 19:10:14 +0300

    Add files via upload

--- additional task.py
+++ additional task.py
@@ -1,7 +1,5 @@
 grades = [[5, 3, 3, 5, 4], [2, 2, 2, 3], [4, 5, 5, 2], [4, 4, 3], [5, 5, 5, 4, 5]]
 students = {'Johnny', 'Bilbo', 'Steve', 'Khendrik', 'Aaron'}
-#print(type(grades))
-#print(type(students))
 students = sorted(students)
 #print(sum(grades[0])/len(grades[0]))
 print(students)
@@ -10,9 +8,9 @@ average_grades_Bilbo = (sum(grades[1])/len(grades[1]))
 average_grades_Johnny = (sum(grades[2])/len(grades[2]))
 average_grades_Khendrik = (sum(grades[3])/len(grades[3]))
 average_grades_Steve = (sum(grades[4])/len(grades[4]))
-average_grades = { }
-average_grades.update ({'Aaron':average_grades_Aaron,
-                        'Bilbo':average_grades_Bilbo,'Johnny':average_grades_Johnny,
-                        'Khendrik':average_grades_Khendrik,'Steve':average_grades_Steve})
-print(average_grades)
 
+average_grades = { }
+
+average_grades_all = [average_grades_Aaron, average_grades_Bilbo, average_grades_Johnny, average_grades_Khendrik, average_grades_Steve]
+average_grades = dict(zip(students, average_grades_all))
+print(average_grades)
