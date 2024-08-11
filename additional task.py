d43942e529c216e71c2279f52c232ccb5fa79464
19Serg84 <pagalin.sergey@yandex.ru>
2024-08-11 19:12:25 +0300

    Add files via upload

--- additional task.py
+++ additional task.py
@@ -8,9 +8,6 @@ average_grades_Bilbo = (sum(grades[1])/len(grades[1]))
 average_grades_Johnny = (sum(grades[2])/len(grades[2]))
 average_grades_Khendrik = (sum(grades[3])/len(grades[3]))
 average_grades_Steve = (sum(grades[4])/len(grades[4]))
-
-average_grades = { }
-
 average_grades_all = [average_grades_Aaron, average_grades_Bilbo, average_grades_Johnny, average_grades_Khendrik, average_grades_Steve]
 average_grades = dict(zip(students, average_grades_all))
 print(average_grades)
